4e300867a047339d04ed7b1adfaf3f95b52a6fde
JakFoster <jakfoster97@outlook.com>
2024-05-10 22:13:40 +0100

    Completed home page almost completed header

--- PlotTwist/src/components/headerComponent.jsx
+++ PlotTwist/src/components/headerComponent.jsx
@@ -12,62 +12,52 @@ export default function Header() {
   //state for the burger bar - so when true, it is showing
   const [showMenuBar, setShowMenuBar] = useState(false);
 
-  const menuBarOptions = [
-    { name: "HOME", image: { homeicon }, link: "/" },
-    { name: "CREATE ROUTE", image: { createroute }, link: "/create-route" },
-    { name: "SAVED ROUTES", image: { savedroutes }, link: "/saved-routes" },
-    { name: "HELP", image: { help }, link: "" },
-  ];
   return (
-    <>
-      <div className="top-bar">
-        <div className="logo-container">
+    <header className="header">
+      {!showMenuBar && (
+        <>
           <Link to={"/"}>
-            <img id="logo" src={logo} alt="logo"></img>
+            <img className="header__logo" src={logo} alt="U-Plot logo"></img>
           </Link>
-        </div>
-
-        <div className="menu-icon-container">
           <button
-            className="menu-button"
+            className="header__openButton"
             onClick={() => {
               setShowMenuBar(!showMenuBar);
               //when click on this, we want the burger menu to show...
-              console.log("hello");
             }}
           >
-            <img id="menu-icon" src={menuicon} alt="menu-icon"></img>
+            <img
+              className="header__burger"
+              src={menuicon}
+              alt="Burger menu icon"
+            ></img>
           </button>
-          {showMenuBar && (
-            <div className="menu-options">
-              <button
-                className="X"
-                onClick={() => {
-                  setShowMenuBar(false);
-                }}
-              >
-                X
-              </button>
-              {menuBarOptions.map((element, index) => (
-                <div className="menu-options-buttons" key={index}>
-                  {element.image.length > 2 && (
-                    <img src={element.image} alt={element.name} />
-                  )}
-                  <Link to={element.link}>
-                    <button
-                      key={index}
-                      className={element.name}
-                      onClick={element.whenClick}
-                    >
-                      {element.name}
-                    </button>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-    </>
+        </>
+      )}
+
+      {showMenuBar && (
+        <nav className="header__navigationMenu">
+          <ul className="header__linksList">
+            <Link className="header__link" to={"/"}>
+              <li className="header__linkItem">HOME</li>
+            </Link>
+            <Link className="header__link" to={"../create-route"}>
+              <li className="header__linkItem">CREATE ROUTE</li>
+            </Link>
+            <Link className="header__link" to={"../saved-routes"}>
+              <li className="header__linkItem">SAVED ROUTES</li>
+            </Link>
+          </ul>
+          <button
+            className="header__closeButton"
+            onClick={() => {
+              setShowMenuBar(false);
+            }}
+          >
+            X
+          </button>
+        </nav>
+      )}
+    </header>
   );
 }
--- PlotTwist/src/routes/createRoutePage/createRoutePage.jsx
+++ PlotTwist/src/routes/createRoutePage/createRoutePage.jsx
@@ -26,39 +26,39 @@ export default function CreateRoutePage() {
   };
   return (
     <>
-      <div className="headerForNow">
-        <Header />
-      </div>
-      <div className="createRoute__OpeningParas">
-        {changeOpeningPara ? (
+      <Header />
+      <main className="mainCreateRoutePage">
+        <div className="createRoute__OpeningParas">
+          {changeOpeningPara ? (
+            <h2>
+              Welcome to U-PLOT! <br /> You will be able to plan routes simply
+              and easily
+            </h2>
+          ) : (
+            <h2>You have reset your route </h2>
+          )}
+          <br />
           <h2>
-            Welcome to U-PLOT! <br /> You will be able to plan routes simply and
-            easily
+            Start by clicking your start and end points, then add any other
+            stops along the way
           </h2>
-        ) : (
-          <h2>You have reset your route </h2>
-        )}
-        <br />
-        <h2>
-          Start by clicking your start and end points, then add any other stops
-          along the way
-        </h2>
-      </div>
-      <div className="createRoute__map">
-        <DynamicMap
-          routeIsCreated={routeIsCreated}
-          handleMapClick={handleMapClick}
-          markerCoordinatesArray={markerCoordinatesArray}
-          setRouteIsCreated={setRouteIsCreated}
-          setMarkerCoordinatesArray={setMarkerCoordinatesArray}
-        />
-      </div>
-      {!routeIsCreated ? (
-        <div className="createRoute__Buttons">
-          <button onClick={handleReset}>Reset</button>
-          <button onClick={handleRouteCreation}> Create Route </button>
         </div>
-      ) : null}
+        <div className="createRoute__map">
+          <DynamicMap
+            routeIsCreated={routeIsCreated}
+            handleMapClick={handleMapClick}
+            markerCoordinatesArray={markerCoordinatesArray}
+            setRouteIsCreated={setRouteIsCreated}
+            setMarkerCoordinatesArray={setMarkerCoordinatesArray}
+          />
+        </div>
+        {!routeIsCreated ? (
+          <div className="createRoute__Buttons">
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleRouteCreation}> Create Route </button>
+          </div>
+        ) : null}
+      </main>
     </>
   );
 }
